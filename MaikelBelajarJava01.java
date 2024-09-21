bb78b6821997a8bc67803e1263c72eb741b10968
Michaelhulu <135936448+Michaelhulu@users.noreply.github.com>
2024-09-21 08:47:56 +0700

    Add files via upload

---
+++ MaikelBelajarJava01.java
@@ -0,0 +1,7 @@
+class BelajarJava01 {
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Maikel Buala Beriman Hulu");
+        System.out.println("Nim :607062430007 ");
+        System.out.println("Kelas d3if-48-01");
+    }
+}
